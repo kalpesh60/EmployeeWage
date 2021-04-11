f3937813f45a2cae74e14a44a8696ec2eb53a8c1
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-04-11 19:37:22 +0530

    added:Day Salary

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,10 +1,15 @@
 echo "Welcome to Employee Wage Computation Program"
 #!/bin/bash
+EmpRatePerHr=20
 empCheck=$((RANDOM%2))
 isPresent=1
 if [ $empCheck -eq $isPresent ]
 then
 	echo "employee is Present"
+EmpHr=8
 else
-	echo "Employee is absent"
+echo "Employee is absent"
+EmpHr=0
 fi
+DaySalary=$(($EmpHr*$EmpRatePerHr))
+echo "day Salary:"$DaySalary
