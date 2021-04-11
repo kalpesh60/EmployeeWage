a1749695479dcd1a0c2f9eb357b1b8bca866099c
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-04-11 19:45:58 +0530

    added:Part-time Salary

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,12 +1,17 @@
 echo "Welcome to Employee Wage Computation Program"
 #!/bin/bash
 EmpRatePerHr=20
-empCheck=$((RANDOM%2))
+empCheck=$((RANDOM%3))
 isPresent=1
+isPartTime=2
 if [ $empCheck -eq $isPresent ]
 then
 	echo "employee is Present"
 EmpHr=8
+elif [ $isPartTime -eq $empCheck ]
+then
+echo "part Time present"
+EmpHr=4
 else
 echo "Employee is absent"
 EmpHr=0
