b2223be0c564412af48013646ff739f195963d08
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-04-11 19:56:13 +0530

    added:Computed Using Switch Case

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -2,19 +2,22 @@ echo "Welcome to Employee Wage Computation Program"
 #!/bin/bash
 EmpRatePerHr=20
 empCheck=$((RANDOM%3))
-isPresent=1
+isFullTime=1
 isPartTime=2
-if [ $empCheck -eq $isPresent ]
-then
-	echo "employee is Present"
+case $empCheck in
+$isFullTime)
+echo "employee is Present"
 EmpHr=8
-elif [ $isPartTime -eq $empCheck ]
-then
+;;
+$isPartTime)
 echo "part Time present"
 EmpHr=4
-else
+;;
+*)
 echo "Employee is absent"
 EmpHr=0
-fi
+;;
+esac
 DaySalary=$(($EmpHr*$EmpRatePerHr))
 echo "day Salary:"$DaySalary
+
