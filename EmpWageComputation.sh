8c16e24c6d9399f3fdc4bb436671c887c23fa45c
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-04-11 20:42:52 +0530

    added: Wages till Hours or days

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -4,9 +4,14 @@ EmpRatePerHr=20
 isFullTime=1
 isPartTime=2
 totalSalary=0
+noOfWorkingDays=20
+MaxHrsMonth=100
+totalEmpHrs=0
+totalWorkingDays=0
 day=1
-while [ $day -le 20 ]
+while [[ $totalEmpHrs -lt $MaxHrsMonth && $totalWorkingDays -lt $noOfWorkingDays ]]
 do
+((totalWorkingDays++))
 empCheck=$((RANDOM%3))
 case $empCheck in
 $isFullTime)
@@ -22,10 +27,11 @@ echo "Employee is absent"
 EmpHr=0
 ;;
 esac
+totalEmpHrs=$(($totalEmpHrs + $EmpHr))
+
 DaySalary=$(($EmpHr*$EmpRatePerHr))
 echo "day Salary:"$DaySalary
 totalSalary=$(($totalSalary+($EmpRatePerHr*$EmpHr)))
-((day++))
 done
 echo "total Salary:" $totalSalary
-
+echo "empHrs:" $totalEmpHrs
